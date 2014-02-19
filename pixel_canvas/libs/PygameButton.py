0115bee6d0f8c1069e0da0023c521aad5867af21
Rahul Anand <rahulanand.fine@gmail.com>
2014-02-19 12:41:35 +0530

    pixel_canvas: SaveLoad module complete!
    TODO: File dialog for saving and loading canvas

--- pixel_canvas/libs/PygameButton.py
+++ pixel_canvas/libs/PygameButton.py
@@ -8,10 +8,6 @@ import pygame
 from pygame.locals import*
 import os
 
-# the current directory is by default from where the code runs
-# change it to current directory of this file for proper resource file laoding
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
 class PygameButton():    
 
     def __init__(self, text, x=0, y=0, w=60, h=20, bgcolor=(255, 255, 255), fontcolor=(0, 0, 0), onclick = None, canvasSurface = None):        
@@ -19,7 +15,9 @@ class PygameButton():
         self.bgcolor = bgcolor
         self.fontcolor = fontcolor        
         pygame.font.init()
-        self.font = pygame.font.Font('Junction.ttf', 14)
+        # the current directory is by default from where the code runs
+        # change it to current directory of this file for proper resource file laoding
+        self.font = pygame.font.Font(os.path.dirname(os.path.realpath(__file__)) + os.sep + 'Junction.ttf', 14)
         self.rect = pygame.Rect(x, y, w, h)
         self.text = text                
         self.onClick = onclick
--- pixel_canvas/plugins/saveload/saveload.py
+++ pixel_canvas/plugins/saveload/saveload.py
@@ -5,6 +5,8 @@
 ##########################################################################################
 
 from libs.PygameButton import PygameButton
+import pickle
+import pygame
 
 class SaveLoad():    
 
@@ -14,39 +16,54 @@ class SaveLoad():
         self.PIXIESIDE = pixieside
         self.GAP = gap
         self.canvas = canvas
+        self.freeMarginY = self.canvas.freeMarginY
         x = self.CANVASSIDE - self.MARGIN + 10
-        y = self.canvas.freeMarginY + 20  
+        y = self.canvas.freeMarginY + 20
         # initialize interface buttons
         self.saveButton = PygameButton("save", x, y, w=60, h=20, onclick=self.save, canvasSurface=self.canvas.canvas)
         self.loadButton = PygameButton("load", x, y+20+5, w=60, h=20, onclick=self.load, canvasSurface=self.canvas.canvas)
+        self.canvas.freeMarginY += 20 + 20 + 5 + 20
             
 
     def save(self):
-        # save in custom format
-        '''
-        Text file:
-        --------------------------------------
-        <savefile identifier>
-        <canvas attributes>
-        <color arrays, space separated>
-        '''
+        # save in custom format        
+        realPixieSide = self.PIXIESIDE + self.GAP        
+        y = 0
+        pc = []
+        while y < self.CANVASSIDE:
+            x = 0
+            while x < (self.CANVASSIDE - self.MARGIN):
+                pc.append(self.canvas.canvas.get_at((x, y)))
+                x += realPixieSide                
+            y += realPixieSide   
+        try:
+            pickle.dump( pc, open("save.pc", "wb") )
+            print "Canvas saved!"
+        except:
+            print "Error in saving the canvas"
+        
+
+    def load(self):
+        # load file and draw on canvas
+        try:
+            pc = pickle.load( open("save.pc", "rb") )  
+            print "Save file loaded!"
+        except:
+            print "Error in loading save file"
+            return
         realPixieSide = self.PIXIESIDE + self.GAP        
         y = 0
         while y < self.CANVASSIDE:
             x = 0
             while x < (self.CANVASSIDE - self.MARGIN):
-                print self.canvas.get_color(x,y)
+                pygame.draw.rect(self.canvas.canvas, pc.pop(0), (x, y, self.PIXIESIDE, self.PIXIESIDE))
                 x += realPixieSide                
-            y += realPixieSide   
-        
-
-    def load(self):
-        # load file and draw on canvas
-        pass      
+            y += realPixieSide
 
         
     def onMouseClick(self, eventObj):
-        # TODO: check margin coordinates and return earlier
+        if (eventObj.pos[0] < self.CANVASSIDE - self.MARGIN or eventObj.pos[1] < self.freeMarginY):
+            return
         self.saveButton.onMouseClick(eventObj)
         self.loadButton.onMouseClick(eventObj)
     
